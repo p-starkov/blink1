4ccc56dd4ac90e863fd8e9c71b589bed5567942f
p-starkov <ps_spam@mail.ru>
2026-06-10 02:07:27 +0300

    2.0.0 - LCD

--- hall-1.cpp
+++ hall-1.cpp
@@ -1,22 +1,44 @@
-
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 const int HALL_PIN = 2;
 //номер пина с подключенным датчиком
 
+int counter = 0;
+//счетчик срабатываний
+
+int lastHallResult = HIGH;
+//последнее состояние датчика
+
+// Укажите адрес дисплея: обычно 0x27 или 0x3F
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
+  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);  // встроенный светодиод - вывод
-  pinMode(HALL_PIN, INPUT); // Холл - ввод
+  pinMode(HALL_PIN, INPUT_PULLUP); // Холл - ввод
+  lastHallResult = digitalRead(HALL_PIN);
+  
+  // Инициализация дисплея
+  lcd.init();
+  
+  // Включить подсветку
+  lcd.backlight();
+    
+  // Вывести текст
+  lcd.print("Privet!");
 }
 
 // the loop function runs over and over again forever
 void loop() {
-    int hallResult;
-    hallResult = digitalRead(HALL_PIN);  // получение значения датчика
-    if (hallResult == HIGH) { // если проищошло срабатывание - включить светодиод
-        digitalWrite(LED_BUILTIN, HIGH);
+    int hallResult = digitalRead(HALL_PIN);  // получение значения датчика
+    if (hallResult == LOW && lastHallResult == HIGH) { // если произошла смена на LOW
+        counter = counter + 1;
+        Serial.println(counter);
+        lcd.clear();
+        lcd.print(String("Count: " + counter));
     }
-
-    delay(1000); //через секунду после проверки светодиод выключить при любом варианте
-    digitalWrite(LED_BUILTIN, LOW);
+    
+    lastHallResult = hallResult;
   
 }
